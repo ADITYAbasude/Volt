e8e16c5702d75a5fbc6f18f4119f7c0f92502e93
frisk2137 <95711852+papaj2139@users.noreply.github.com>
2025-09-15 20:53:20 +0200

    Update linux_build.sh

--- linux_build.sh
+++ linux_build.sh
@@ -1,25 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
 #go to project root directory
 cd "$(dirname "$0")"
 
-#create build directory if it doesn't exist
-mkdir -p build
-cd build
+#makes the script exit on error
+set -e
 
-#configure CMake
-echo "== Configuring CMake =="
-#set CMAKE_PREFIX_PATH to help CMake find QT 6
-cmake -DCMAKE_PREFIX_PATH=$(qmake6 -query QT_INSTALL_PREFIX) ..
-if [ $? -ne 0 ]; then
-    echo "CMake configuration failed!"
-    exit 1
-fi
+#configure and build
+echo "== Configuring and Building Project =="
+cmake -S . -B build -G Ninja
+cmake --build build --parallel
 
-#build the editor
-echo "== Building Project =="
-cmake --build . --parallel $(nproc)
-if [ $? -ne 0 ]; then
-    echo "Build failed!"
-    exit 1
-fi
+echo "== Build completed successfully =="
